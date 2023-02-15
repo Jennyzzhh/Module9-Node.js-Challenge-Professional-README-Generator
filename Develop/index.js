969c652ffb2d4f60ca3cf570940d4f8db455cc0d
Jennyzzhh <zzh19980929@gmail.com>
2023-02-14 20:12:36 -0500

    add to gemerateMarkdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,92 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+let questions = inquirer
+.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'What is your description?',
+      },
+      {
+        type: 'input',
+        name: 'instructions',
+        message: 'What is your installation instructions?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What is your usage information?',
+      },
+      {
+        type: 'input',
+        name: 'guidelines',
+        message: 'What is your contribution guidelines?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What is your test instructions?',
+      },
+      {
+        type: 'list',
+        message: 'What license do you prefer',
+        name: 'license',
+        choices: ['Apache', 'Boost', 'BSD', 'CC0'],
+      },
+      {
+        type: 'input',
+        message: 'What are the test procedures',
+        name: 'test',
+      },
+      {
+        type: 'input',
+        message: 'How to contribute',
+        name: 'contribute',
+      },
+      {
+        type: 'input',
+        name: 'Github',
+        message: 'What is your GitHub username ?',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+      }
+])
+.then((response) => {
+console.log(response)
+writeToFile("README.md", generateMarkdown(response));
+}
+);
+// .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+// questions.forEach((string)=>writeToFile(string))
+// // TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile('Readme.md',data, (err) =>
+    err ? console.error(err) : console.log('Data wrtten to the file successfully')
+    ); 
+}
 
 // TODO: Create a function to initialize app
 function init() {}
